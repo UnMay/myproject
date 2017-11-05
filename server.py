d45cca0cf5f545ae200feb849194fee005c0d913
UnMay <necromant650@ya.ru>
2017-11-05 15:20:24 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,28 +1,34 @@
-import socket
-import os
+import socket #подключаем сокет
+import os #подклюаем модуль для работы с os
 
-TCP_IP = 'localhost'
-TCP_PORT = 8000
-BUFFER_SIZE = 1024
+host = "localhost" #адрес
+port = 8000 #порт
+buffer_size = 1024 #размер буффера
+current_directory = os.getcwd()#текущая рабочая директория
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP, TCP_PORT))
-s.listen(10)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #создаем сокет
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.bind((host, port)) #связываем сокет с хостом и портом
+sock.listen(1) #запускаем режим прослушивания. Аргумент - максимальное количество подключений в очереди (1)
 
+#Общаемся с клиентом:
 while True:
-    conn, address = s.accept()
-    data = conn.recv(BUFFER_SIZE)
-    request = data.split('\r\n',1)[0].split(' ')[1]
+	conn, addr = sock.accept()  # принимаем подключение
+	request = conn.recv(buffer_size) # получить данные
 
-    if address == "/about/aboutme.html":
-        file = open("/home/lisa/Documents/"+address, mode = 'r')
-        conn.send("""HTTP/1.1 200 OK \n Content type:text HTML\n\n\n """ + file.read()) 
-	file.close() 
+	try:
+		address = str(request).split(' ')[1]#получить адресс страницы из запроса
+	except:
+		address = ""
+	print(address)
 
-    if address == "/index.html" or "/":
-        file = open("/home/lisa/Documents/index.html", mode = 'r')
-	conn.send("""HTTP/1.1 200 OK \n Content type:text HTML\n\n\n """ + file.read())  
-	file.close()
-			
-conn.close()
-s.close()
+	if (os.path.exists(current_directory+address)):  #возвращает True, если path указывает на существующий путь или дескриптор открытого файла.
+		if address == "/":
+			address="/index.html"
+		file = open(current_directory+address, 'r')
+		conn.send(("HTTP/1.1 200 OK \n Content type:text HTML\n\n\n " + file.read()).encode())
+		file.close()
+	else:
+		conn.send(b"HTTP/1.1 404 Not Found\n") #сообщить, что запрошенной страницы нет
+	conn.close()
+sock.close()
